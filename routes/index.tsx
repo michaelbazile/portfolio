06dda3d1b5088c0a7d58ceadb175d86f3513d096
Michael Bazile <mr.michaelbazilejr@gmail.com>
2023-01-31 11:49:52 -0600

    refactor: remove unused code from home component

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,24 +1,21 @@
 import { Head } from "$fresh/runtime.ts";
 import Counter from "../islands/Counter.tsx";
+import { Header } from "../components/Header.tsx";
+import { Footer } from "../components/Footer.tsx";
 
-export default function Home() {
+const Home = () => {
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>Michael Bazile Portfolio</title>
       </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the
-          ./routes/index.tsx file, and refresh
-        </p>
-        <Counter start={3} />
+      <div>
+        <Header active="" />
+
+        <Footer children />
       </div>
     </>
   );
-}
+};
+
+export default Home;
