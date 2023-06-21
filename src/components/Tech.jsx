e001a99d9062b53381e70f6a512df91306458719
Michael Bazile <mr.michaelbazilejr@gmail.com>
2023-06-21 06:07:53 -0500

    add imports to start developing tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
 const Tech = () => {
-  return (
-    <div>Tech</div>
-  )
-}
+  return <div>Tech</div>;
+};
 
-export default Tech
+export default SectionWrapper(Tech, "tech");
