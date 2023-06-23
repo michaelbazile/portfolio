0d0c250dc40f881f4d694e20bb1f98a4fd3990e5
Michael Bazile <mr.michaelbazilejr@gmail.com>
2023-06-23 09:57:14 -0500

    remove div to fix navbar spacing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,17 +29,20 @@ const Navbar = () => {
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
-          {navLinks.map((link) => (
-            <li
-              key={link.id}
-              className={`${
-                active === link.title ? "text-white" : "text-secondary"
-              } hover:text-white text-[18px font-medium cursor-pointer]`}
-              onClick={() => setActive(link.title)}
-            >
-              <a href={`${link.id}`}>{link.title}</a>
-            </li>
-          ))}
+          {navLinks.map((link) => {
+            console.log(link);
+            return (
+              <li
+                key={link.id}
+                className={`${
+                  active === link.title ? "text-white" : "text-secondary"
+                } hover:text-white text-[18px font-medium cursor-pointer]`}
+                onClick={() => setActive(link.title)}
+              >
+                <a href={`#${link.id}`}>{link.title}</a>
+              </li>
+            );
+          })}
         </ul>
         <div className="sm:hidden flex flex-1 justify-end items-center">
           <img
@@ -71,8 +74,6 @@ const Navbar = () => {
             </ul>
           </div>
         </div>
-
-        <div></div>
       </div>
     </nav>
   );
